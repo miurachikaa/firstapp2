b456a61d36bef0d3094a5a2a858cd05cce9df153
miurachikaa <chikaa719@gmail.com>
2023-04-29 13:56:20 +0900

    Initial commit

---
+++ src/main/java/in/techcamp/firstapp2/FirstApp2Application.java
@@ -0,0 +1,13 @@
+package in.techcamp.firstapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/firstapp2/FirstApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.firstapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
