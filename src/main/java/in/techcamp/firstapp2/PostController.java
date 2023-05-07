cc010b4b1cb328929134ebee0814c863da8e3b2c
miurachikaa <chikaa719@gmail.com>
2023-05-07 10:12:58 +0900

    リスト表示

--- src/main/java/in/techcamp/firstapp2/PostController.java
+++ src/main/java/in/techcamp/firstapp2/PostController.java
@@ -1,7 +1,10 @@
 package in.techcamp.firstapp2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -11,7 +14,13 @@ public class PostController {
     }
 
     @GetMapping
-    public String showList(){
+    public String showList(Model model) {
+        var postList = List.of(
+                new PostEntity(1, "投稿１"),
+                new PostEntity(2, "投稿２"),
+                new PostEntity(3, "投稿３")
+        );
+        model.addAttribute("postList", postList);
         return "index";
     }
 }
